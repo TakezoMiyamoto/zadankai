15790d6dd52c9d0367a0a4b248a04cfbe6f5e1a6
takezomiyamoto <takezo.m.1987@gmail.com>
2016-03-03 14:21:23 +0000

    validates Project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
   belongs_to :user
-  
+  validates :project_name, :category, presence: true
   mount_uploader :main_image, ImageUploader
   mount_uploader :movie, MovieUploader
 
