b9eac6046c5131006a6da5954e4b46a2597191e4
takezomiyamoto <takezo.m.1987@gmail.com>
2016-03-05 10:21:52 +0000

    #9 modify project and user model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
   validates :project_name, :category, presence: true
   mount_uploader :main_image, ImageUploader
   mount_uploader :movie, MovieUploader
