34405921460fbd5a60d6b1b5ff147e9b084b9c5d
takezomiyamoto <takezo.m.1987@gmail.com>
2016-03-05 10:23:06 +0000

    #9 modify comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
+  validates :user_id, presence: true
+  validates :comment, presence: true, length: { maximum: 200 }
 end
