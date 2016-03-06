ee4e4b14b17dc0a7bfc08e074d420bcd319d8f92
takezomiyamoto <takezo.m.1987@gmail.com>
2016-03-06 13:38:31 +0000

    #15 add edit and update method in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,35 @@
 class UsersController < ApplicationController
+  
+  before_action :set_user, only:[:show, :edit, :update]
+  
   def index
     @users = User.all
   end
 
   def show
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    if @user.update(edit_user_params)
+      flash[:success] = "Your profile was updated!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  
+  def edit_user_params
+    params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :avatar)
+  end
+  
+  def set_user
     @user = User.find(params[:id])
   end
 end
