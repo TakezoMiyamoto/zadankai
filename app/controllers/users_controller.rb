4bfa35b86a4bfb19ae1bff60958fea07bf9ec043
takezomiyamoto <takezo.m.1987@gmail.com>
2016-03-12 05:49:32 +0000

    #34 see my projects and jonned projects in my page view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,10 @@ class UsersController < ApplicationController
   end
 
   def show
+    # 自分のプロジェクト一覧
     @projects = @user.projects.group('projects.id')
-    
+    # 参加中のプロジェクト一覧
+    @joinProjects = @user.joined_projects.group('projects.id')
   end
   
   def edit
